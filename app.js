a6585a9129e6eab475f555510ecb0ffe17e1874b
prakritish <bhattacharyaprakritish@gmail.com>
2026-01-17 20:58:28 +0530

    Mock data collect...

--- app.js
+++ app.js
@@ -1,62 +1,79 @@
-// import React from "react";
-// import ReactDOM from "react-dom/client";
 
-// // React Element using React.createElement
-// const heading = React.createElement("h1", 
-//     {id: "heading"} , 
-//     "Namaste React"
-// );
-// /*=============================
-//   JSX Element Renderiong
-//   =============================*/
-// // create a Htmlelement using JSX
-// const jsxHeading = <h1 className = "heading">Namaste React using JSX</h1>
-// const loremText = (
-//     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis illum iure error maiores laudantium? Quibusdam dignissimos nobis doloribus veniam unde culpa ipsa aliquid, quo deserunt quasi harum officiis ea nesciunt.
-//     Pariatur beatae mollitia, ullam dolorum sed et repellat ea consequatur perferendis, exercitationem quam ipsa, dignissimos ab. Facere quae minus reiciendis delectus laboriosam incidunt porro minima optio, impedit atque eligendi rerum?
-//     Ducimus ea expedita placeat reiciendis sint ipsam quaerat, doloribus deserunt laudantium numquam error molestiae veniam quibusdam minima sed sunt consectetur omnis, mollitia quidem accusantium autem iure. Quo enim sed minima.</p>
-// )
-
-// /*=============================
-//   React Functional Component
-//   =============================*/
-// const HeadingComponent = () =>(
-//      <h1>Namaste React Functional Component</h1>
-// )
-// const root = ReactDOM.createRoot(document.getElementById("root"));
-
-// root.render(heading);
-// root.render(jsxHeading);
-// root.render(loremText);
-// root.render(<HeadingComponent/>)
 import  React from "react"
 import {createRoot} from "react-dom/client"
 
-// React element
-const heading = React.createElement("h1",{id: "heading"}, "fucking react")
-
-//  heading using JSX
-// const jsxHeading = <h1>Fucking JSX</h1>
-
-/*-----------------------------
-  Components
-    ->Functional Component (NEW)
-            const HeadingComponent = () =>{
-                return <h1>React functional component</h1>
-            }
-    ->Class Based Component (OLD)
--------------------------------*/
-const HeadingComponent = () =>{
-                return <h1>React functional component</h1>
+//  Header component
+const Header = ()=>{
+  return(
+    <div className="header">
+      <div>
+        <img className="logo" src="black-pic.jpg"></img>
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact Us</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  )
 }
-const FooterComponent = ()=>(
-  <div className="footer">
-    <HeadingComponent/>
-    <h1>Hello I am the footer</h1>
-  </div>
-)
+// ReataurantCard component
+const ReataurantCard = ({resName,cuisine,rating})=>{
+  return(
+    <div className="res-card">
+      <img alt ="res-logo" className="res-logo" src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/kcew9m5qdv7vfuurvxyx"></img>
+      <h3>{resName}</h3>
+      <h4>{cuisine}</h4>
+      <h4>{rating}</h4>
+      <h4>38 minutes</h4>
+    </div>
+  )
+}
+// Body component
+const Body = () =>{
+  return(
+    <div className="body">
+      <div className="search">Search</div>
+      <div className="res-container">
+        <ReataurantCard
+          resName = "Meghana Foods"
+          cuisine = "Biriyani, North Indian, Asian"
+          rating = "4.4 stars"
+        />
+        <ReataurantCard 
+          resName = "KFC"
+          cuisine = "Burger, Fast Food"
+          rating = "4.5 stars"
+        />
+        <ReataurantCard
+          resName = "Food Caffee"
+          cuisine = "Burger, Fast Food"
+          rating = "4.1 stars"
+        />
+        {/* <ReataurantCard/>
+        <ReataurantCard/>
+        <ReataurantCard/>
+        <ReataurantCard/>
+        <ReataurantCard/>
+        <ReataurantCard/>
+        <ReataurantCard/>
+        <ReataurantCard/>
+        <ReataurantCard/> */}
+      </div>
+    </div>
+  )
+}
+const AppLayout = ()=>{
+  return(
+    <div className="app">
+        <Header/>
+        <Body/>
+    </div>
+  )
+}
+
 const root = createRoot(document.getElementById("root"))
-// root.render(heading)
-// root.render(jsxHeading)
-// root.render(<HeadingComponent/>)
-root.render(<FooterComponent/>) 
+root.render(<AppLayout/>) 
